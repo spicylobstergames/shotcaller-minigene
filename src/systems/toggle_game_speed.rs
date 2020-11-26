5e35b9760e204482e3b3449899dab3f195222ab8
Erlend Sogge Heggen <e.soghe@gmail.com>
2020-11-26 11:14:09 +0100

    Update toggle_game_speed.rs
    Halved it.

--- src/systems/toggle_game_speed.rs
+++ src/systems/toggle_game_speed.rs
@@ -9,7 +9,7 @@ system!(
         for k in events.read(&mut res.0) {
             if k == &InputEvent::SpeedToggle {
                 if speed.0 == 1.0 {
-                    speed.0 = 4.0;
+                    speed.0 = 2.0;
                 } else {
                     speed.0 = 1.0;
                 }
