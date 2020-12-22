8ed60c2863e85b978f1ef93fd637f8dda38c74df
Joël Lupien (Jojolepro) <jojolepro@jojolepro.com>
2020-12-22 16:51:04 -0500

    remove useless system params.

--- src/systems/creep_spawner.rs
+++ src/systems/creep_spawner.rs
@@ -1,20 +1,10 @@
 use crate::*;
 
 system!(
-    CreepSpawnerSystem,
-    |entities: Entities<'a>,
+    CreepSpawnerSystem, |
      positions: WriteStorage<'a, Point>,
      spawners: WriteStorage<'a, CreepSpawner>,
-     creeps: WriteStorage<'a, Creep>,
-     simple_movements: WriteStorage<'a, SimpleMovement>,
-     ai_destinations: WriteStorage<'a, AiDestination>,
-     proximity_attacks: WriteStorage<'a, ProximityAttack>,
-     stats: WriteStorage<'a, Comp<StatSet<Stats>>>,
-     stat_def: ReadExpect<'a, StatDefinitions<Stats>>,
-     ai_paths: WriteStorage<'a, AiPath>,
      teams: WriteStorage<'a, Team>,
-     sprites: WriteStorage<'a, Sprite>,
-     sprite_indices: WriteStorage<'a, SpriteIndex>,
      game_events: Write<'a, EventChannel<GameEvent>>| {
         for (pos, mut spawner, team) in (&positions, &mut spawners, &teams).join() {
             if spawner.0 == 0 {
