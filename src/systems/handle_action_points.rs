0fdc8df5b9e539666afca8f85bb374865b1c68fa
Owen McCormick <oweninoregon@gmail.com>
2020-12-24 08:25:30 -0800

    Put stat storage into join in handle_action_points

--- src/systems/handle_action_points.rs
+++ src/systems/handle_action_points.rs
@@ -7,20 +7,20 @@ system!(
     simple_movements: ReadStorage<'a, SimpleMovement>,
     hero1_simple_movements: ReadStorage<'a, Hero1SimpleMovement>,
     targets: WriteStorage<'a, AiDestination>| {
-        for (e, _) in (&*entities, &simple_movements).join() {
-            stats.get_mut(e).unwrap().0.stats.get_mut(&Stats::ActionPoints).unwrap().value +=
-                stats.get(e).unwrap().0.stats.get(&Stats::ActionPointRefillRate).unwrap().value;
-            if stats.get(e).unwrap().0.stats.get(&Stats::ActionPoints).unwrap().value >= 100.0 {
-                stats.get_mut(e).unwrap().0.stats.get_mut(&Stats::ActionPoints).unwrap().value -= 100.0;
+        for (e, stat, _) in (&*entities, &mut stats, &simple_movements).join() {
+            stat.0.stats.get_mut(&Stats::ActionPoints).unwrap().value +=
+                stat.0.stats.get(&Stats::ActionPointRefillRate).unwrap().value;
+            if stat.0.stats.get(&Stats::ActionPoints).unwrap().value >= 100.0 {
+                stat.0.stats.get_mut(&Stats::ActionPoints).unwrap().value -= 100.0;
             } else {
                 targets.remove(e);
             }
         }
-        for (e, _) in (&*entities, &hero1_simple_movements).join() {
-            stats.get_mut(e).unwrap().0.stats.get_mut(&Stats::ActionPoints).unwrap().value +=
-                stats.get(e).unwrap().0.stats.get(&Stats::ActionPointRefillRate).unwrap().value;
-            if stats.get(e).unwrap().0.stats.get(&Stats::ActionPoints).unwrap().value >= 100.0 {
-                stats.get_mut(e).unwrap().0.stats.get_mut(&Stats::ActionPoints).unwrap().value -= 100.0;
+        for (e, stat, _) in (&*entities, &mut stats, &hero1_simple_movements).join() {
+            stat.0.stats.get_mut(&Stats::ActionPoints).unwrap().value +=
+                stat.0.stats.get(&Stats::ActionPointRefillRate).unwrap().value;
+            if stat.0.stats.get(&Stats::ActionPoints).unwrap().value >= 100.0 {
+                stat.0.stats.get_mut(&Stats::ActionPoints).unwrap().value -= 100.0;
             } else {
                 targets.remove(e);
             }
