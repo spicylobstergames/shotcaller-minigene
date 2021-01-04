d05adc945b60aa7d8e19f2ceaacc312f29a4cd77
Joël Lupien (Jojolepro) <jojolepro@jojolepro.com>
2021-01-04 11:45:56 -0500

    use action pool refill rate with effectors

--- src/systems/handle_action_points.rs
+++ src/systems/handle_action_points.rs
@@ -4,7 +4,7 @@ use crate::*;
 pub fn handle_action_points_system(stats: &mut Components<StatSet<Stats>>) -> SystemResult {
     // TODO consider using an effector instead?
     for stat in stats.iter_mut() {
-        let refill = stat.stats.get(&Stats::ActionPointRefillRate).unwrap().value;
+        let refill = stat.stats.get(&Stats::ActionPointRefillRate).unwrap().value_with_effectors;
         let stat = stat.stats.get_mut(&Stats::ActionPoints).unwrap();
         stat.value += refill;
         if stat.value >= 100.0 {
