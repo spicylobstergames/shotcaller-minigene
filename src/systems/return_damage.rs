21ff1ab976558a5a293d7fd9f677fe421c65135c
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2021-01-21 13:57:17 -0300

    Fix bug when damaged entity has no skills

--- src/systems/return_damage.rs
+++ src/systems/return_damage.rs
@@ -8,8 +8,10 @@ pub fn return_damage_system(
     let mut return_damage = None;
     for ev in game_events.iter() {
         if let GameEvent::DamageEntity(a, t, dmg) = ev {
-            if skills.get(*t).unwrap().skills.get(&Skills::ReturnDamage).is_some() {
-                return_damage = Some(GameEvent::DamageEntity(*t, *a, dmg / 2.0));
+            if let Some(skills) = skills.get(*t) {
+                if skills.skills.get(&Skills::ReturnDamage).is_some() {
+                    return_damage = Some(GameEvent::DamageEntity(*t, *a, dmg / 2.0));
+                }
             }
         }
     }
