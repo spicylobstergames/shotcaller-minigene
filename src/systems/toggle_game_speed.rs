fafdbbaad5175dd980b804fce517c3b2269acf0f
jojolepro <jojolepro@jojolepro.com>
2021-05-05 16:48:56 -0400

    add speed toggle to 4.0.

--- src/systems/toggle_game_speed.rs
+++ src/systems/toggle_game_speed.rs
@@ -5,9 +5,7 @@ pub fn toggle_game_speed_system(events: &Vec<InputEvent>, time: &mut Time) -> Sy
     for k in events.iter() {
         if k == &InputEvent::SpeedToggle {
             if time.time_scale() == 1.0 {
-                // TODO toggle back to 2.0
-                //speed.0 = 2.0;
-                time.set_time_scale(0.0);
+                time.set_time_scale(4.0);
             } else {
                 time.set_time_scale(1.0);
             }
