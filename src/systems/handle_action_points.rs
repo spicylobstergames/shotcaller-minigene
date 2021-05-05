a0c03680dd0b43b8690f2905d51cbf93c27fb9ca
jojolepro <jojolepro@jojolepro.com>
2021-05-05 16:20:59 -0400

    make handle action points time dependent.

--- src/systems/handle_action_points.rs
+++ src/systems/handle_action_points.rs
@@ -1,13 +1,13 @@
 use crate::*;
 
 /// Refills the action points of entities using their `ActionPointRefillRate` stat.
-pub fn handle_action_points_system(stats: &mut Components<StatSet<Stats>>) -> SystemResult {
+pub fn handle_action_points_system(time: &Time, stats: &mut Components<StatSet<Stats>>) -> SystemResult {
     for stat in stats.iter_mut() {
         let refill = stat
             .stats
             .get(&Stats::ActionPointRefillRate)
             .unwrap()
-            .value_with_effectors;
+            .value_with_effectors * time.delta_time().as_secs_f64();
         let stat = stat.stats.get_mut(&Stats::ActionPoints).unwrap();
         stat.value += refill;
         if stat.value >= 100.0 {
