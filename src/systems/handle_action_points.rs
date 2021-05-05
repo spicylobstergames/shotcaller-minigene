49295ceb863752ecd2d1c0d47990fd0f1596e99c
jojolepro <jojolepro@jojolepro.com>
2021-05-05 16:32:04 -0400

    rustfmt.

--- src/systems/handle_action_points.rs
+++ src/systems/handle_action_points.rs
@@ -1,13 +1,17 @@
 use crate::*;
 
 /// Refills the action points of entities using their `ActionPointRefillRate` stat.
-pub fn handle_action_points_system(time: &Time, stats: &mut Components<StatSet<Stats>>) -> SystemResult {
+pub fn handle_action_points_system(
+    time: &Time,
+    stats: &mut Components<StatSet<Stats>>,
+) -> SystemResult {
     for stat in stats.iter_mut() {
         let refill = stat
             .stats
             .get(&Stats::ActionPointRefillRate)
             .unwrap()
-            .value_with_effectors * time.delta_time().as_secs_f64();
+            .value_with_effectors
+            * time.delta_time().as_secs_f64();
         let stat = stat.stats.get_mut(&Stats::ActionPoints).unwrap();
         stat.value += refill;
         // TODO replace manual clamp to max by handling inside of the stat.
