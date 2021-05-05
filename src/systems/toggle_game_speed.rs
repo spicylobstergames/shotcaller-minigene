34d5e9334bc249088fef3464fc309a5d774a2c37
jojolepro <jojolepro@jojolepro.com>
2021-05-05 12:51:19 -0400

    set that time toggle stops time.

--- src/systems/toggle_game_speed.rs
+++ src/systems/toggle_game_speed.rs
@@ -5,7 +5,9 @@ pub fn toggle_game_speed_system(events: &Vec<InputEvent>, speed: &mut GameSpeed)
     for k in events.iter() {
         if k == &InputEvent::SpeedToggle {
             if speed.0 == 1.0 {
-                speed.0 = 2.0;
+                // TODO toggle back to 2.0
+                //speed.0 = 2.0;
+                speed.0 = 0.0;
             } else {
                 speed.0 = 1.0;
             }
