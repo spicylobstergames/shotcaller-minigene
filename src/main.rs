bb476f87384b99ece4fd5c83a20b8ab4c866582e
jojolepro <jojolepro@jojolepro.com>
2021-05-15 11:02:12 -0400

    create nakama file.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ use std::collections::HashMap;
 use nakama_rs::api_client::{Event, ApiClient};
 use nakama_rs::matchmaker::{Matchmaker, QueryItemBuilder};
 
+mod nakama;
+
 add_wasm_support!();
 
 const PLAY_WIDTH: u32 = 81;
@@ -562,38 +564,6 @@ fn main() -> BError {
 
     world.get_mut::<Camera>().unwrap().size.x = PLAY_WIDTH as i32;
 
-    /*let mut nakama = ApiClient::new("defaultkey", "127.0.0.1", 7350, "http");
-    nakama.register("email", "password", "username");
-    nakama.authenticate("email", "password");
-    nakama.authenticated();
-    nakama.username().unwrap();
-
-    let mut matchmaker = Matchmaker::new();
-    matchmaker.min(2).max(2).add_string_property("engine", "minigene_matchmaking")
-        .add_query_item(&QueryItemBuilder::new("engine").required().term("minigene_matchmaking").build());
-    nakama.socket_add_matchmaker(&matchmaker);
-
-    let token = nakama.matchmaker_token.clone();
-    if token.is_some() {
-        nakama.socket_join_match_by_token(&token.unwrap());
-    }
-    if nakama.match_id().is_some() {
-        // start game here
-    }
-    //nakama.socket_send(opcode, &data);
-    while let Some(event) = nakama.try_recv() {
-        match event {
-            Event::Presence {joins, leaves} => {
-                //leaver.session_id
-                //join.session_id
-                //join.username
-            },
-            Event::MatchData {user_id, opcode, data} => {
-                //DeBin::deserialize_bin(&data).expect("Failed to deser received data");
-            }
-        }
-    }
-    //nakama.tick();*/
 
     let gs = State {
         world,
